dcdff934299b5e5ff5a397ddb934950241a73367
Timothy Okooboh <okoobohtimothy@gmail.com>
2020-04-24 14:40:09 +0100

    deploy

---
+++ precache-manifest.507785f914e8b47eea97b96414b184f5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100ef121dbf85eed0a13",
+    "url": "/dictionary/css/404.a0a392d0.css"
+  },
+  {
+    "revision": "5eb5d1fad8b51c69816f",
+    "url": "/dictionary/css/about.63c480c4.css"
+  },
+  {
+    "revision": "4397057af2126fcd5856",
+    "url": "/dictionary/css/app.2a1931d9.css"
+  },
+  {
+    "revision": "b53ff55e09a092f9f0cbbf74af01b715",
+    "url": "/dictionary/img/github.b53ff55e.svg"
+  },
+  {
+    "revision": "371d8f0f31387c5858572eadaaf9e5eb",
+    "url": "/dictionary/img/search.371d8f0f.svg"
+  },
+  {
+    "revision": "677dc202b262f9b720b9f7080b2ea4d8",
+    "url": "/dictionary/img/speaker.677dc202.svg"
+  },
+  {
+    "revision": "8335fedc4238992596dd3cbe7bb1345f",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "100ef121dbf85eed0a13",
+    "url": "/dictionary/js/404.5cbd9f03.js"
+  },
+  {
+    "revision": "5eb5d1fad8b51c69816f",
+    "url": "/dictionary/js/about.c9fa414e.js"
+  },
+  {
+    "revision": "4397057af2126fcd5856",
+    "url": "/dictionary/js/app.7c0c81a1.js"
+  },
+  {
+    "revision": "4f24955d7b5fe793f3b7",
+    "url": "/dictionary/js/chunk-vendors.6b88fcc2.js"
+  },
+  {
+    "revision": "f8fedfbd0f8565d928f8a08001049f4d",
+    "url": "/dictionary/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dictionary/robots.txt"
+  }
+]);
